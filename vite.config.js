1bafc9b67219c809efae5d330f539f6d22442005
morningistare <iamfromhell66@gmail.com>
2025-09-27 14:49:37 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+base: '/Quiz-buddy-v2/'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
